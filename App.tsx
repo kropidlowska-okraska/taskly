761f957ecd9c86735c83c5925cfd1771920258df
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-02-12 20:46:17 +0100

    Add ShoppingListItem component

--- App.tsx
+++ App.tsx
@@ -1,33 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
-import { theme } from "./theme";
+import { StyleSheet, View } from "react-native";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert("Delete", "Are you sure you want to delete this item?", [
-      { text: "Cancel", style: "cancel" },
-      {
-        text: "Delete",
-        onPress: () => console.log("I am deleting"),
-        style: "destructive",
-      },
-    ]);
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>Kawa</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleDelete}
-          accessibilityRole="button"
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
+      <ShoppingListItem name="Kawa" />
+      <ShoppingListItem name="Herbata" />
+      <ShoppingListItem name="Cukier" />
     </View>
   );
 }
@@ -37,26 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-  },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
-    paddingHorizontal: 8,
-    paddingVertical: 16,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  itemText: { fontSize: 18, fontWeight: 200 },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
   },
 });
