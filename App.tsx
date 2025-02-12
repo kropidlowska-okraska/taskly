0fe936127350c5d7db69a792a721e913d974c8b7
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-02-12 20:06:19 +0100

    Create first view with Cancel button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Delete", "Are you sure you want to delete this item?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Delete",
+        onPress: () => console.log("I am deleting"),
+        style: "destructive",
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Kawa</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDelete}
+          accessibilityRole="button"
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +36,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1a759f",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  itemText: { fontSize: 18, fontWeight: 200 },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
