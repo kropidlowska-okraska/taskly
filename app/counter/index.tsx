8bac19171d75d7e23c43e6c9bae02ad1912e8262
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-04 13:31:19 +0100

    Schedule notifications and add a counter

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,21 +1,54 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
 
 export default function CounterScreen() {
-  const handlePermission = async () => {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsElapsed((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const scheduleNotification = async () => {
     const result = await registerForPushNotificationsAsync();
-    console.log("Permission result", result);
+
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "No hejka 🎉",
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Unable to schedule notfication",
+          "You need to enable notifications in your settings to use this feature.",
+        );
+      }
+    }
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>Seconds elapsed: {secondsElapsed}</Text>
       <TouchableOpacity
         style={styles.button}
-        onPress={handlePermission}
+        onPress={scheduleNotification}
         activeOpacity={0.8}
       >
-        <Text style={styles.buttonText}>Request Permission</Text>
+        <Text style={styles.buttonText}>Schedule notification</Text>
       </TouchableOpacity>
     </View>
   );
@@ -32,6 +65,10 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorBlack,
     padding: 12,
     borderRadius: 6,
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 24,
   },
   buttonText: {
     color: theme.colorWhite,
